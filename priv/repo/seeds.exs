c7f74c9e780259ec0c47109f1c9762c0392df58e
techno-tanoC <develop.tanoc@gmail.com>
2017-01-22 12:08:43 +0900

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,7 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     MyTube.Repo.insert!(%MyTube.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias MyTube.{Repo, Item}
+
+Repo.delete_all(Item)
+
+%Item{title: "choco fondue", url: "https://www.youtube.com/watch?v=9u7itnyLFbc"} |> Item.changeset |> Repo.insert!
+%Item{title: "Hotel Moonside", url: "https://www.youtube.com/watch?v=e76bgm5NM0k"} |> Item.changeset |> Repo.insert!
+%Item{title: "Snail's House - Candy Dash (VIP) feat.ストロベリー症候群", url: "https://www.youtube.com/watch?v=oHbvkAVeD_M"} |> Item.changeset |> Repo.insert!
