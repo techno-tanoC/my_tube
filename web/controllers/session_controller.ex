df2598cbc65ca7852fa7379eddc0299e34603768
techno-tanoC <develop.tanoc@gmail.com>
2017-01-23 19:15:29 +0900

    login

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -7,19 +7,24 @@ defmodule MyTube.SessionController do
     render conn, "new.html"
   end
 
-  def create(conn, _params) do
-    token = SecureRandom.base64(64)
-    changeset = ApiKey.changeset(%ApiKey{}, %{"token" => token})
+  def create(conn, %{"session" => params}) do
+    auth = Application.get_env(:my_tube, MyTube.Auth)
+    if params["name"] == auth[:name] && params["pass"] == auth[:pass] do
+      token = SecureRandom.base64(64)
+      changeset = ApiKey.changeset(%ApiKey{}, %{"token" => token})
 
-    case Repo.insert(changeset) do
-      {:ok, api_key} ->
-        conn
-        |> put_status(:created)
-        |> render("show.json", api_key: api_key)
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(MyTube.ChangesetView, "error.json", changeset: changeset)
+      case Repo.insert(changeset) do
+        {:ok, api_key} ->
+          conn
+          |> put_status(:created)
+          |> render("show.json", api_key: api_key)
+        {:error, changeset} ->
+          conn
+          |> put_status(:unprocessable_entity)
+          |> render(MyTube.ChangesetView, "error.json", changeset: changeset)
+      end
+    else
+      send_resp(conn, :unauthorized, "")
     end
   end
 
