668583c1d96f31e363e22813d0ea0d276ae12715
techno-tanoC <develop.tanoc@gmail.com>
2017-01-23 13:46:57 +0900

    session view

--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -2,10 +2,6 @@ defmodule MyTube.SessionView do
   use MyTube.Web, :view
 
   def render("show.json", %{api_key: api_key}) do
-    %{data: render_one(api_key, MyTube.SessionView, "token.json")}
-  end
-
-  def render("api_key.json", %{api_key: api_key}) do
-    %{token: api_key.token}
+    %{data: %{token: api_key.token}}
   end
 end
